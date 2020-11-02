34f1269fe6ce19e7edf6e962196c181da0678660
Chetan <chetandesai2488@gmail.com>
2020-11-02 16:13:32 +0530

    Working on news details screen

--- DRC_Practical_Test/ViewController/ViewController.swift
+++ DRC_Practical_Test/ViewController/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "News"
         getNewsUpdate()
     }
 
@@ -37,14 +38,52 @@ class ViewController: UIViewController {
             if self != nil {
                 print("\(self!.objNewsViewModel.arrListArticles)")
                 print(self!.objNewsViewModel.arrListArticles[0].author as Any)
-                //self!.tblNewsList.reloadData()
+                self!.tblNewsList.reloadData()
             }
         }
         
         setProgressHub(view: self.view)
         objNewsViewModel.newsListCall()
     }
-    
-
 }
 
+// MARK: UICollection view methods
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return objNewsViewModel.arrListArticles.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NewsTableViewCell
+        cell.lblTitle.text = objNewsViewModel.arrListArticles[indexPath.section].title
+        cell.lblAuthorName.text = objNewsViewModel.arrListArticles[indexPath.section].author
+        cell.lblDate.text = objNewsViewModel.arrListArticles[indexPath.section].publishedAt
+        //cell.delegate = self
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 8))
+        footerView.backgroundColor = .clear
+        return footerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tblNewsList.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
